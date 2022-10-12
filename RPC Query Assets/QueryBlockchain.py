f86e62b58497284feb18940ff991ac961d2420d1
QuodSumusHocEritis <31145452+QuodSumusHocEritis@users.noreply.github.com>
2022-10-11 23:02:24 -0500

    Updates to Query Blockchain
    Additional routines for clarity, added objects for Block & Transaction.  Need to figure out how to filter and append pertinent transaction information

--- RPC Query Assets/QueryBlockchain.py
+++ RPC Query Assets/QueryBlockchain.py
@@ -2,31 +2,49 @@ from jsonrpcclient import request, parse, Ok
 import logging
 import requests
 #Program description
-#This program exists to monitor the attacker and it's transactions.  This program needs to query the blockchain and each transaction for each address.
-  
+#This program exists to monitor the attacker and its transactions.  This program needs to query the blockchain and each transaction for each address.
+
 #Variables
 intCurrBlock = 0
 strRpcAddr = 'http://<RPC Username Here>:<RPC Password Here>@<RPC IP Here>:<RPC Port Here>/'
 
-def GetBlockCount():
-    response = requests.post(strRpcAddr, json=request("getblockcount"))
-    #print(response)
-    return ParseResponse(response)
+#Classes
+class Transaction:
+    def __init__(self):
+        self.Hash = ''
+        self.Sender = ''
+        self.Recipient = ''
+        self.Amount = 0
 
-def GetEntireChain(intCurrBlockNum):
-    dictBlockchainInfo = {}
-    for intCounter in range(intCurrBlockNum):        
-        respGetBlkHash = requests.post(strRpcAddr, json=request("getblockhash", params=(intCounter,))) 
-        strCurrBlockHash = ParseResponse(respGetBlkHash)       
-        respCurrBlock = requests.post(strRpcAddr, json=request("getblock", params=(str(strCurrBlockHash), True,)))
-        dictBlockchainInfo[strCurrBlockHash] = ParseResponse(respCurrBlock)
-        print("Curr block hash: " + str(strCurrBlockHash) + "---" + "Block Info: " + str(dictBlockchainInfo[strCurrBlockHash]))
-    return dictBlockchainInfo
 
+
+class Block:
+    def __init__(self, pHash, pBlockNum):
+        self.BlockNum = pBlockNum
+        self.Hash = pHash
+        #An empty tuple
+        self.TXs = ()
+
+    def AddTx(self, pTransaction):
+        self.TXs = self.TXs + (pTransaction,)
+
+
+#Functions
 def GetSpecifiedBlockHash(intBlockNum):
-    print(intBlockNum)
-    response = requests.post(strRpcAddr, json=request("getblockhash", params=(intBlockNum,)))
-    return ParseResponse(response)
+    return SendCommand("getblockhash", (intBlockNum,))
+    
+def GetBlockCount():
+    return SendCommand("getblockcount")
+    
+def GetRawTx(pTxId):
+    return SendCommand("getrawtransaction", (pTxId, True,))
+    
+def GetBlockInfo(pBlockHash):
+    return SendCommand("getblock", (pBlockHash, True))  
+    
+def ParseTxIdsDataFromGetBlock(pGetBlockResp):
+    lstTxIds = pGetBlockResp["tx"]
+    return lstTxIds
     
 def ParseResponse(pResponse):
     parsed = parse(pResponse.json())
@@ -35,18 +53,50 @@ def ParseResponse(pResponse):
     else:
         return logging.error(parsed.message)
 
-def SendCommandNoParams(strCommand):
-    response = requests.post(strRpcAddr, json=request(strCommand))
-    return ParseResponse(response)
+def GetHelpText(strCommand):
+    return SendCommand("help", (strCommand,))
     
-def SendCommandWithParams(strCommand, param1):
-    response = requests.post(strRpcAddr, json=request(strCommand, params=(param1,)))
+def SendCommand(strCommand, pParams = ()):
+    response = requests.post(strRpcAddr, json=request(strCommand, params = pParams)) 
     return ParseResponse(response)
 
+#Get chain info from genesis to specified block number
+def GetChainFromGenTo(intBlockNum):
+    tupBlockchainInfo = ()
+    for intCounter in range(intBlockNum + 1):  
+        #Get block hash of specified block number      
+        strCurrBlockHash = GetSpecifiedBlockHash(intCounter) 
+        objBlock = Block(strCurrBlockHash, intCounter) 
+        #Get block info of specified block hash     
+        lstCurrBlockInfo = GetBlockInfo(strCurrBlockHash)
+        #Parse out transactions in from block info
+        lstTxData = ParseTxIdsDataFromGetBlock(lstCurrBlockInfo)
+        #Add to block hash transaction information to dictionary as a kvp 
+        for x in lstTxData:
+            objNewTx = Transaction()
+            objNewTx.Hash = x
+            objBlock.AddTx(objNewTx)
+        tupBlockchainInfo = tupBlockchainInfo + (objBlock,)
+    return tupBlockchainInfo
+
+def GetTxInfo(pTxId):
+   return SendCommand("getrawtransaction", (pTxId, 1,))
+
+#Main body
 #First, get the block count
 BlockCount = GetBlockCount()
-print("The block count is: " + str(BlockCount))
+print("Curr Block Count: " + str(BlockCount))
 CurrBlockHash = GetSpecifiedBlockHash(BlockCount)
-print("GetSpecifiedBlockHash() output is: " + str(CurrBlockHash))
-EntireChain = GetEntireChain(BlockCount)
-print(SendCommandWithParams("help", "getblock"))
+print("Curr Block Hash: " + str(CurrBlockHash))
+
+#We really want the entire chain.  This var is simply to make testing less cumbersome.
+tupBlocksAndTx = GetChainFromGenTo(100)
+for currBlock in tupBlocksAndTx:
+    print("Block # " + str(currBlock.BlockNum) + " (Block Hash: " + currBlock.Hash + ")")
+    print("Transaction List:")
+    for currTx in currBlock.TXs:
+        print("- " + currTx.Hash)
+        print(GetTxInfo(currTx.Hash))
+
+
+print(GetHelpText("getrawtransaction"))
